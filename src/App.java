85741ae3d7a8c7e66b2aab687bbd26a071eabe7a
sungbaekheo <sungbaek.heo@gmail.com>
2022-09-02 17:05:21 +0900

    first commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int val = 5;
+        int val_02 = 10;
+        System.out.println("Hello, World!");
+        int val_03 = 20;
+    }
+}
