480e2b9ee483281a19570ac582065f9ec23299e7
sungbaekheo <sungbaek.heo@gmail.com>
2022-09-05 09:38:14 +0900

    First step to project

---
+++ src/MiniProjectConnetMysql.java
@@ -0,0 +1,12 @@
+public class MiniProjectConnetMysql {
+    public static void main(String[] args) {
+        // Connect to database
+            // Access to SQL server
+            // Check host name
+            // Check user
+            // Check Schema
+            // Check password
+
+    }
+    
+}
